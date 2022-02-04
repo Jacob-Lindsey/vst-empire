83b935136cf33563277b8269902ba7635b37875f
Jake <jacob.lindsey88@gmail.com>
2022-02-04 14:19:34 -0600

    Made Navbar menu collapsible and responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,41 +1,49 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useGlobalContext } from "../../store/context/context";
-import styles from "./Navbar.module.css";
-import Cart from "../Cart/Cart";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../../store/context/context'
+import { IoCartSharp } from 'react-icons/io5'
+import styles from './Navbar.module.css'
+import Cart from '../Cart/Cart'
 
 const Navbar = () => {
+  const [cartOpen, setCartOpen] = useState(false)
 
-    const [cartOpen, setCartOpen] = useState(false);
+  const { state } = useGlobalContext()
 
-    const { state } = useGlobalContext();
+  const cartSize = state.cart.reduce(function (acc, obj) {
+    return acc + obj.quantity
+  }, 0)
+  const handleShowCart = () => {
+    setCartOpen(!cartOpen)
+  }
 
-    const cartSize = state.cart.reduce(function (acc, obj) {
-        return acc + obj.quantity
-    }, 0);
-    const handleShowCart = () => {
-        setCartOpen(!cartOpen);
-    };
+  return (
+    <nav className={styles.wrapper}>
+      <input className={styles.menuBtn} type='checkbox' id='menuBtn' />
+      <label className={styles.menuIcon} htmlFor='menuBtn'>
+        <span className={styles.navIcon}></span>
+      </label>
+      <ul className={styles.navList}>
+        <Link className={styles.link} to='/'>
+          <li className={styles.navLink}>Home</li>
+        </Link>
+        <li className={styles.navLink}>Link2</li>
+        <li className={styles.navLink}>Link3</li>
+        <li
+          onClick={() => handleShowCart()}
+          className={`${styles.navLink} ${styles.cartButton}`}
+        >
+          <IoCartSharp size={28} />
+          <span>{cartSize}</span>
+        </li>
+      </ul>
+      {cartOpen ? (
+        <div className={styles.cartContainer}>
+          <Cart />
+        </div>
+      ) : null}
+    </nav>
+  )
+}
 
-    return (
-        <nav className={styles.wrapper}>
-            <ul className={styles.navList}>
-                <Link className={styles.link} to='/'>
-                    <li className={styles.navLink}>Home</li>
-                </Link>
-                <li className={styles.navLink}>Link2</li>
-                <li className={styles.navLink}>Link3</li>
-                <li onClick={() => handleShowCart()} className={styles.navLink}>
-                    Cart
-                    <span>{cartSize}</span>
-                </li>
-            </ul>
-            {
-                cartOpen ? <div className={styles.cartContainer}><Cart /></div> : null
-            }
-            
-        </nav>
-    )
-};
-
-export default Navbar;
+export default Navbar
--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,35 +1,31 @@
-import { useLocation } from "react-router";
-import styles from "./ProductDetail.module.css";
+import { useLocation } from 'react-router'
+import styles from './ProductDetail.module.css'
 
 const ProductDetail = () => {
+  const location = useLocation()
+  const { item } = location.state
 
-    const location = useLocation();
-    const { item } = location.state;
+  return (
+    <section className={styles.container}>
+      <div className={styles.detailWrapper}>
+        <img
+          className={styles.image}
+          src={`https://via.placeholder.com/400/222222/DDDDDD/?text=${item.item.name}`}
+          alt=''
+        />
+        <div className={styles.descriptionWrapper}>
+          <p className={styles.name}>{item.item.name}</p>
+          <span className={styles.brand}>By: {item.item.brand}</span>
+          <p className={styles.description}>{item.item.description}</p>
+        </div>
+        <div className={styles.priceWrapper}>
+          <p className={styles.priceLabel}>Price</p>
+          <span className={styles.price}>{item.item.price}</span>
+          <button className={styles.button}>ADD TO CART</button>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-    return (
-        <section className={styles.container}>
-            <div className={styles.detailWrapper}>
-                <img  className={styles.image} src={`https://via.placeholder.com/220x150/222222/DDDDDD/?text=${item.name}`} alt='' />
-                <div className={styles.descriptionWrapper}>
-                    <p className={styles.name}>{item.name}</p>
-                    <p className={styles.description}>{item.description}</p>
-                </div>
-                <div className={styles.priceWrapper}>
-                    <span>
-                        <p className={styles.priceLabel}>Price</p>
-                        <span className={styles.price}>{item.price}</span>
-                        <button className={styles.button}>ADD TO CART</button>
-                    </span>
-                </div>
-                
-            </div>
-            
-            <div>
-                {item.item.name}
-            </div>
-        </section>
-    )
-
-};
-
-export default ProductDetail;
+export default ProductDetail
